44331d0c439f5ad848057085a929d211b8f0d9f7
Sang Nguyen <sangnguyendinh24work@gmail.com>
2021-11-25 11:50:17 +0700

    Test phpunit student function

--- database/connectDatabase.php
+++ database/connectDatabase.php
@@ -1,3 +1,12 @@
 <?php
+require '../../vendor/autoload.php';
+$client = new MongoDB\Client(
+  'mongodb+srv://sangnd:A2LCgZecgEmsRd2A@nhattan.7vquo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+);
 
-
+// Connect to the "data" database
+if ($data = $client->data){
+  // echo "Connected";
+}else{
+  echo "Error: " . $data->error;
+}
